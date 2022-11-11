39e6f1b1dd22ebfbe48f7332edd18bd97da15088
PavelAlekseev <pavelbooking.com@yandex.ru>
2022-11-11 19:14:16 +0500

    mesto

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,32 @@ import profileImage from '../images/image.jpg';
 import addCardButton from '../images/Vector2.svg';
 
 function Main() {
+    function handleEditAvatarClick() {
+        document.querySelector('.popup_avatar').classList.add('popup_opened');
+      }
+    
+      function handleEditProfileClick() {
+        document.querySelector('.popup_type_profile').classList.add('popup_opened');
+      }
+
+      function handleAddPlaceClick() {
+        document.querySelector('.popup_type_photo').classList.add('popup_opened');
+      }
+
     return(
         <main>
             <section className="profile">
                 <div className="profile__content">
                     <div className="profile__avatar">
-                        <button className="profile__editpic"></button>
+                        <button className="profile__editpic" onClick={handleEditAvatarClick}></button>
                         <img className="profile__image" src={profileImage} alt="Жак-Ив Кусто"></img>
                     </div>
                     <div className="profile__name-box">
                         <h1 className="profile__name">Жак-Ив Кусто</h1>
-                        <button className="profile__button button-show" type="button" aria-label="кнопка редактирования профиля"></button>
+                        <button className="profile__button button-show" type="button" aria-label="кнопка редактирования профиля" onClick={handleEditProfileClick}></button>
                         <p className="profile__profession">Исследователь океана</p>
                     </div>
-                    <button className="profile__button-photo button-show" type="button" aria-label="кнопка добавления фото">
+                    <button className="profile__button-photo button-show" type="button" aria-label="кнопка добавления фото" onClick={handleAddPlaceClick}>
                         <img src={addCardButton} alt="кнопка добавления фото"></img>
                     </button>
                 </div>
@@ -25,7 +37,8 @@ function Main() {
                 </ul>
             </section>
         </main>
-    );
+    )
+
 }
 
 export default Main;
